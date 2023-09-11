e9fde31c1dadf0c5aa8690489ef28fe2389931df
mohamedshamil7 <mohamedshamil0507@gmail.com>
2023-09-11 13:40:18 +0530

    Auth bug fixed signIn signup bug fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NetflixLogoURL } from '../utils/constants'
 import { signout } from '../utils/hooks/authHooks'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/appStore'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useDispatch } from 'react-redux'
+import { auth } from '../utils/firebase/frebaseCongif'
+import { addUser, removeUser } from '../store/slice/userSlice'
 
 const Header = () => {
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const user = useSelector((store:RootState)=>store.user)
   const handleClick = ()=>{
@@ -14,9 +19,25 @@ const Header = () => {
       alert( response?.errorMessage)
       return
     }
-    navigate("/")
 
   }
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //user SignIn/SingUp
+        const {uid,email,displayName} = user;
+        dispatch(addUser({uid,email,displayName}))
+        navigate("/browse")
+      } else {
+        // User is signed out
+        dispatch(removeUser())
+        navigate("/")
+
+      }
+    });
+  },[])
+
   return (
     <div 
     className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between' >
